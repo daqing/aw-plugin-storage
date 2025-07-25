3df5fd59e0deba65396d315f361be9e550c3ccb8
David Zhang <daqing@mzevo.com>
2025-07-25 15:44:16 +0800

    Add storage plugin

---
+++ app/api/storage_api/routes.go
@@ -0,0 +1,14 @@
+package storage_api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Routes(r *gin.RouterGroup) {
+	g := r.Group("/storage")
+	{
+		g.POST("/upload", UploadAction)
+		g.POST("/delete", DeleteAction)
+		g.GET("/serve/*path", ServeAction)
+	}
+}
